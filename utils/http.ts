287c5a3ea132cf6df5549f672bd55a9812c01fb7
Pedro Gardim <me@pedrogardim.com>
2024-06-25 19:20:17 +0200

    add custom versions of useQuery and useMutation hooks

--- utils/http.ts
+++ utils/http.ts
@@ -1,10 +1,23 @@
-import axios from "axios";
+import axios, { AxiosHeaders } from "axios";
 import { QueryClient } from "react-query";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import type { QueryFunctionContext, QueryKey } from "react-query";
 
+const supabaseApiUrl = process.env.EXPO_PUBLIC_SB_API_REST_URL || "";
+const supabaseApiKey = process.env.EXPO_PUBLIC_SB_API_KEY || "";
+
 const defaultQueryFn = async ({ queryKey }: QueryFunctionContext<QueryKey>) => {
-  const { data } = await axios.get(`https://api.github.com${queryKey[0]}`);
+  const jwtToken = await AsyncStorage.getItem("token");
+  const headers = new AxiosHeaders();
+  headers.set("apiKey", supabaseApiKey);
+
+  if (jwtToken) {
+    headers.set("Authorization", `Bearer ${jwtToken}`);
+  }
+  console.log(jwtToken);
+  const { data } = await axios.get(supabaseApiUrl + queryKey[0], { headers });
+
   return data;
 };
 
