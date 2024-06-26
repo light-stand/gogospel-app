4485d5e7cb8f5f27d19dead890de50ae0cd4f6f4
Pedro Gardim <me@pedrogardim.com>
2024-06-27 01:50:30 +0200

    add basic profile type screen

---
+++ constants/profiling.ts
@@ -0,0 +1,22 @@
+import { Option } from "@/components/ui/forms/Select/Select";
+
+export const profilingFlow = {
+  misionary: ["name", "bio", "picture", "interests"],
+  ministery: ["name", "bio", "picture"],
+};
+
+export const profileTypeOptions: Option[] = [
+  {
+    title: "Misionero",
+    value: "missionary",
+    description: "Soy misionero y busco a un ministerio donde servir a Dios.",
+    icon: "hand-heart",
+  },
+  {
+    title: "Ministerio",
+    value: "ministry",
+    description:
+      "Soy un ministerio que busca misioneros para llevar a cabo su misión.",
+    icon: "church",
+  },
+];
