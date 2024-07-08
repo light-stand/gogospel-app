b97332649a1748c48b25979c411a7ad1929208e1
Pedro Gardim <me@pedrogardim.com>
2024-07-08 23:29:44 +0200

    change mission creation route name

---
+++ app/mission/(creation)/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Text } from "@/components";
+
+export default function MissionCreation() {
+  return (
+    <Container>
+      <Text className="mt-4 text-gray-800">Explore</Text>
+    </Container>
+  );
+}
