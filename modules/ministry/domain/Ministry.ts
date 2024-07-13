9e06f80b4c6b2d7ce3abb7812e8003cc84b75af6
Pedro Gardim <me@pedrogardim.com>
2024-07-13 10:27:48 +0200

    add ministry/missionary modules extending from base repo

---
+++ modules/ministry/domain/Ministry.ts
@@ -0,0 +1,11 @@
+export type Ministry = {
+  id: number;
+  user_id: string;
+  name: string;
+  description: string;
+  lat?: number;
+  lng?: number;
+  images: string[];
+  created_at: string;
+  updated_at: string;
+};
---
+++ modules/missionary/domain/Missionary.ts
@@ -0,0 +1,10 @@
+export type Missionary = {
+  id: number;
+  user_id: string;
+  first_name: string;
+  last_name: string;
+  bio: string;
+  images: string[];
+  created_at: string;
+  updated_at: string;
+};
