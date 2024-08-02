0a2ee34b0a3807681748a5a1b34e3e37a465295d
Pedro Gardim <me@pedrogardim.com>
2024-08-03 01:02:01 +0200

    maps: geocoding types

--- modules/maps/interface/mapsApi.ts
+++ modules/maps/interface/mapsApi.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { LatLng } from "react-native-maps";
+import { ReverseGeocodeResponse } from "../domain/Geocoding";
 
 export const geocode = async (address: string) => {
   const response = await axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
@@ -11,13 +12,12 @@ export const geocode = async (address: string) => {
   return response.data.results[0].geometry;
 };
 
-export const reverseGeocode = async (coords: LatLng | undefined) => {
-  if (!coords) return;
+export const reverseGeocode = async (coords: LatLng) => {
   const response = await axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
     params: {
       key: process.env.EXPO_PUBLIC_GOOGLE_MAP_API_KEY,
       latlng: `${coords.latitude},${coords.longitude}`,
     },
   });
-  return response.data.results[0].formatted_address;
+  return response.data.results[0] as ReverseGeocodeResponse;
 };
