fab2d1d38e4200ac30938e22d86cf519add21dae
Pedro Gardim <me@pedrogardim.com>
2024-08-05 20:39:40 +0200

    collapsable fixes

--- common/components/ui/structure/Collapsable/Collapsable.tsx
+++ common/components/ui/structure/Collapsable/Collapsable.tsx
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
 import { View, ViewProps } from "react-native";
-import { IconButton } from "@/components/ui/actions/IconButton";
-
+import clsx from "clsx";
 import { LinearGradient } from "expo-linear-gradient";
+
+import { IconButton } from "@/components/ui/actions/IconButton";
 
 export interface CollapsableProps extends ViewProps {
   children: React.ReactNode;
   height?: number;
 }
 
-const Collapsable: React.FC<CollapsableProps> = ({ children, height = 200, ...props }) => {
+const Collapsable: React.FC<CollapsableProps> = ({ children, height = 200, style, ...props }) => {
   const [collapsed, setCollapsed] = useState(true);
   return (
     <View
       className="overflow-hidden"
-      style={{ maxHeight: collapsed ? height : undefined }}
+      style={[{ maxHeight: collapsed ? height : undefined }, style]}
       {...props}
     >
       {children}
       <LinearGradient
         colors={collapsed ? ["#ffffff00", "#ffffff99", "#fff"] : []}
-        className="absolute bottom-0 w-full h-8"
+        className={clsx(collapsed && "absolute bottom-0", "w-full h-8")}
       >
         <IconButton
           icon={collapsed ? "chevron-down" : "chevron-up"}
