d8d4e0ceddf48840c3f912c684cf23d878870480
Pedro Gardim <me@pedrogardim.com>
2024-10-07 22:43:17 +0200

    fix: patch distance into mission

--- modules/mission/application/useMissionDetails.ts
+++ modules/mission/application/useMissionDetails.ts
@@ -1,10 +1,15 @@
 import { useQuery } from "react-query";
 
+import { getLocation } from "@/maps/interface/mapsService";
+import { haversineDistance } from "@/maps/utils/distance";
 import { missionRepository } from "@/mission/interface/missionRepository";
 import { useUserStore } from "@/user/store/useUserStore";
+import { useEffect, useState } from "react";
+import { queryClient } from "@/interface/queryClient";
 
 export const useMissionDetails = (id: number) => {
   const { user } = useUserStore();
+
   const { data: mission, isLoading } = useQuery({
     queryKey: ["mission", id],
     queryFn: () =>
@@ -13,6 +18,23 @@ export const useMissionDetails = (id: number) => {
         "*, user_profile!created_by(user_id, name, images), favorite(user_id)"
       ),
   });
+
+  const getMissionDistance = async () => {
+    const userLocation = await getLocation();
+    if (!userLocation || !mission?.lat || !mission?.long) return;
+    const distance = haversineDistance(
+      userLocation?.latitude,
+      userLocation?.longitude,
+      mission.lat,
+      mission.long
+    );
+    mission.distance = Math.floor(distance);
+    queryClient.setQueryData(["mission", id], mission);
+  };
+
+  useEffect(() => {
+    getMissionDistance();
+  }, [mission]);
 
   // Thanks to RLS, "favorite" should contain only the user entry
   const isFavorite =
