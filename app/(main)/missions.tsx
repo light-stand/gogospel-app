e324f841f4711175f9f61c0d0db969bf64f51f13
Pedro Gardim <me@pedrogardim.com>
2024-10-09 19:35:08 +0200

    feature: add button bar reusable component

--- app/(main)/missions.tsx
+++ app/(main)/missions.tsx
@@ -1,8 +1,7 @@
-import { View } from "react-native";
 import { useTranslation } from "react-i18next";
 import { useRouter } from "expo-router";
 
-import { Button, Container, IconButton, Text } from "@/components";
+import { Container, IconButton, Text, ButtonBar } from "@/components";
 import { useListMissions } from "@/mission/application/useListMissions";
 import { MissionList } from "@/mission/components/MissionList";
 import { NoResults } from "@/components/ui/feedback/NoResults";
@@ -26,20 +25,16 @@ export default function Missions() {
   return (
     <Container className="p-0">
       <Text className="font-bold text-3xl m-4 mb-0">{t("screen.missions")}</Text>
-      <View className="flex-row justify-around w-full gap-2 p-2">
-        {missionListTypes.map((type) => (
-          <Button
-            key={type}
-            className="text-xs flex-1 py-1 px-1"
-            label={t(`mission.list.types.${type}`)}
-            size="small"
-            icon={listTypesIcons[type]}
-            variant={mode === type ? "primary" : "text"}
-            onPress={() => setMode(type)}
-            block={false}
-          />
-        ))}
-      </View>
+      <ButtonBar
+        className="p-2 px-4"
+        options={missionListTypes.map((type) => ({
+          label: t(`mission.list.types.${type}`),
+          value: type,
+          icon: listTypesIcons[type],
+        }))}
+        selected={mode}
+        onPress={setMode}
+      />
       <MissionList
         onRefresh={refetch}
         missions={missions || []}
---
+++ common/components/ui/actions/ButtonBar/ButtonBar.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, ViewProps } from "react-native";
+
+import { MaterialIconType } from "../../foundation/Icon/Icon";
+import Button from "../Button/Button";
+
+type ButtonBarOption<T extends string> = {
+  label: string;
+  value: T;
+  icon: MaterialIconType;
+};
+
+export interface ButtonBarProps<T extends string> extends ViewProps {
+  options: ButtonBarOption<T>[];
+  selected: string;
+  onPress: (value: T) => void;
+}
+
+const ButtonBar = <T extends string>({ selected, options, onPress, style }: ButtonBarProps<T>) => {
+  return (
+    <View className="flex-row justify-around w-full gap-2" style={style}>
+      {options.map(({ label, icon, value }: ButtonBarOption<T>) => (
+        <Button
+          className="text-xs flex-1 py-1 px-1"
+          label={label}
+          size="small"
+          icon={icon}
+          variant={selected === value ? "primary" : "text"}
+          onPress={() => onPress(value)}
+          block={false}
+        />
+      ))}
+    </View>
+  );
+};
+
+export default ButtonBar;
