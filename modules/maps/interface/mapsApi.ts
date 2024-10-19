f42e9434e4f511c4681c21934e876985bd1f588b
Pedro Gardim <me@pedrogardim.com>
2024-10-19 11:41:40 +0200

    feature: add autocomplete api

--- modules/maps/interface/mapsApi.ts
+++ modules/maps/interface/mapsApi.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import { LatLng } from "react-native-maps";
 import { ReverseGeocodeResponse } from "../domain/Geocoding";
 
+export type AutoCompleteResult = { description: string; place_id: string };
+
 export const geocode = async (address: string) => {
   const response = await axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
     params: {
@@ -20,4 +22,26 @@ export const reverseGeocode = async (coords: LatLng) => {
     },
   });
   return response.data.results[0] as ReverseGeocodeResponse;
+};
+
+export const autoComplete = async (input: string, coords: LatLng) => {
+  if (!input || input.length < 3) return [];
+  const response = await axios.get("https://maps.googleapis.com/maps/api/place/autocomplete/json", {
+    params: {
+      key: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      input,
+    },
+  });
+  return response.data.predictions as AutoCompleteResult[];
+};
+
+export const getPlaceDetails = async (placeId: string) => {
+  const response = await axios.get("https://maps.googleapis.com/maps/api/place/details/json", {
+    params: {
+      key: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      placeid: placeId,
+      fields: "geometry,address_components",
+    },
+  });
+  return response.data.result;
 };
